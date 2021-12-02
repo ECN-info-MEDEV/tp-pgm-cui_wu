09006c370798737bd5b32fa3fdd0fff151b7774a
Tigris <tigris@MacBook-Pro.local>
2021-12-02 11:07:53 +0100

    Input from terminal

--- tp_pgm_git/src/main/java/org/centrale/projet/medev/tp_pgm_git/HelloWorld.java
+++ tp_pgm_git/src/main/java/org/centrale/projet/medev/tp_pgm_git/HelloWorld.java
@@ -5,14 +5,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.*;
+import java.io.*;
 /**
  *
  * @author tigris
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        PGM testPGM = new PGM("/Users/tigris/EI3/MEDEV/TP3/ImagesTestPGM/baboon.pgm");
+        System.out.println("Please enter the path to the image (.pgm)");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        PGM testPGM = new PGM(input);
         testPGM.read();
         testPGM.thresholding(150);
     }
